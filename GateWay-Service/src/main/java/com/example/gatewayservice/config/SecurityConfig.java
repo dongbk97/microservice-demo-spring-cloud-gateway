5e67a2fbe7110e44c1932de9ff6f1ef9b02b5ab4
dongbk97 <dongbk97>
2023-03-27 21:37:50 +0700

    commit dong_dev

--- GateWay-Service/src/main/java/com/example/gatewayservice/config/SecurityConfig.java
+++ GateWay-Service/src/main/java/com/example/gatewayservice/config/SecurityConfig.java
@@ -1,44 +1,28 @@
 package com.example.gatewayservice.config;
 
-import com.example.gatewayservice.jwt.JwtEntryPointCs;
-import com.example.gatewayservice.jwt.JwtFilterCus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
-@Configuration
-@EnableWebFluxSecurity
+//@Configuration
+//@EnableWebFluxSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private JwtFilterCus jwtFilterCus;
-    @Autowired
-    private JwtEntryPointCs jwtEntryPointCs;
-    @Bean
-    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
-        http
-                .cors()
-                .and()
-                .csrf()
-                .disable()
-                .exceptionHandling()
-                .authenticationEntryPoint(jwtEntryPointCs)
-                .and()
-                .addFilterBefore(jwtFilterCus, SecurityWebFiltersOrder.AUTHORIZATION);
+//    @Autowired
+//    private JwtFilterCus jwtFilterCus;
+//    @Autowired
+//    private JwtEntryPointCs jwtEntryPointCs;
+
+//    @Bean
+//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+//        return http
 //                .authorizeExchange()
-//                .pathMatchers("/**").permitAll()
-//                .anyExchange().authenticated();
-
-        return http.build();
-    }
-
+//                .anyExchange().authenticated()
+//                .and()
+//                .exceptionHandling()
+//                .authenticationEntryPoint(jwtEntryPointCs)
+//                .and()
+//                .addFilterBefore( jwtFilterCus, SecurityWebFiltersOrder.AUTHORIZATION)
+//                .build();
+//    }
 
 }
--- GateWay-Service/src/main/java/com/example/gatewayservice/jwt/JwtFilterCus.java
+++ GateWay-Service/src/main/java/com/example/gatewayservice/jwt/JwtFilterCus.java
@@ -1,50 +1,120 @@
 package com.example.gatewayservice.jwt;
 
+import com.example.gatewayservice.exceptionhandle.JwtTokenMalformedException;
+import io.jsonwebtoken.Claims;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
 
-@Component
-@Slf4j
-public class JwtFilterCus implements WebFilter {
-    @Autowired
-    private JwtUtils jwtUtils;
+//@Component
+//@Slf4j
+public class JwtFilterCus /*implements WebFilter*/ {
+//    @Autowired
+//    private JwtUtils jwtUtil;
+//
+//    private static final String AUTHORIZATION_HEADER = "Authorization";
+//    private static final String BEARER_PREFIX = "Bearer ";
+//
+//
+//
+////    @Override
+////    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+////        ServerHttpRequest request = exchange.getRequest();
+////
+////
+////            if (this.isAuthMissing(request))
+////                return this.onError(exchange, "Authorization header is missing in request", HttpStatus.UNAUTHORIZED);
+////
+////            final String token = this.getAuthHeader(request);
+////
+////            if (jwtUtil.validateJwtToken(token))
+////                return this.onError(exchange, "Authorization header is invalid", HttpStatus.UNAUTHORIZED);
+////
+//////            this.populateRequestWithHeaders(exchange, token);
+////
+////        return chain.filter(exchange);
+////    }
+//
+//
+//    /*PRIVATE*/
+//
+//    private Mono<Void> onError(ServerWebExchange exchange) {
+////        ServerHttpResponse response = exchange.getResponse();
+////        response.setStatusCode(httpStatus);
+////        return response.setComplete();
+//        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+//        exchange.getResponse().getHeaders().add("WWW-Authenticate", "Bearer realm=\"example\"");
+//        exchange.getResponse().getHeaders().add("Content-Type", "application/json");
+//        String message = "{\"error\": \"" + "" + "\"}";
+//        DataBuffer buffer = exchange.getResponse().bufferFactory().wrap(message.getBytes());
+//        return exchange.getResponse().writeWith(Mono.just(buffer)).then(Mono.empty());
+//    }
+//
+//    private String getAuthHeader(ServerHttpRequest request) {
+//        return request.getHeaders().getOrEmpty("Authorization").get(0);
+//
+//    }
+//
+//    private boolean isAuthMissing(ServerHttpRequest request) {
+//        return !request.getHeaders().containsKey("Authorization");
+//    }
+//
+//    private void populateRequestWithHeaders(ServerWebExchange exchange, String token) {
+//        Claims claims = jwtUtil.getClaimsFromToken(token);
+//        exchange.getRequest().mutate()
+//                .header("id", String.valueOf(claims.get("id")))
+//                .header("role", String.valueOf(claims.get("role")))
+//                .build();
+//    }
+//
+//
+//    @Override
+//    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+//        ServerHttpRequest request = exchange.getRequest();
+//
+//
+//        if (this.isAuthMissing(request))
+//            return this.onError(exchange);
+//
+//        final String token = this.parseJwt(exchange);
+//
+//        try {
+//            if (!jwtUtil.validateJwtToken(token)){
+//                return this.onError(exchange);
+//            }
+//        } catch (JwtTokenMalformedException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//
+////            return this.onError(exchange, "Authorization header is invalid", HttpStatus.UNAUTHORIZED);
+//
+////            this.populateRequestWithHeaders(exchange, token);
+//
+//        return chain.filter(exchange);
+//    }
+//
+//    private String parseJwt(ServerWebExchange exchange) {
+//        ServerHttpRequest request = exchange.getRequest();
+//        String responseHeader = request.getHeaders().getOrEmpty("Authorization").get(0);
+//        log.warn("responseHeader: " + responseHeader);
+//
+//        if (StringUtils.hasText(responseHeader) && responseHeader.startsWith("Bearer ")) {
+//            return responseHeader.substring(7, responseHeader.length());
+//
+//        }
+//        return null;
+//    }
 
-    private static final String AUTHORIZATION_HEADER = "Authorization";
-    private static final String BEARER_PREFIX = "Bearer ";
-
-
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-
-        String authHeader = exchange.getRequest().getHeaders().getFirst(AUTHORIZATION_HEADER);
-
-        if (authHeader != null && authHeader.startsWith(BEARER_PREFIX)) {
-            // Lấy token từ header
-            String token = authHeader.substring(BEARER_PREFIX.length());
-
-            // Validate token
-
-            log.error("Token is invalid: " + jwtUtils.validateJwtToken(token));
-
-            if (jwtUtils.validateJwtToken(token)) {
-                // Token hợp lệ, lưu thông tin người dùng vào SecurityContext
-                Authentication auth = new JwtAuthenticationToken(jwtUtils.getUserFromToken(token));
-                return chain.filter(exchange).contextWrite(ReactiveSecurityContextHolder.withAuthentication(auth));
-            }
-        }
-
-        // Nếu token không hợp lệ hoặc không có token, trả về null để chặn request
-
-        return null;
-    }
 }
 
 
